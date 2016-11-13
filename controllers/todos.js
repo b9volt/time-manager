a62be6e7390f240be4a648688b7a5a2916747282
Brad Volts <b9volt@aol.com>
2016-11-12 16:42:53 -0800

    Adding more dependancies, create/add, and show
    Updated dependancies.
    Added create/add to app.js and begin build on controllers.
    Added forms and controllers in index.html.

--- controllers/todos.js
+++ controllers/todos.js
@@ -3,4 +3,42 @@ var router = express.Router();
 
 var Todo = require('../models/todo.js');
 
+router.get('/', function(req, res){
+  var query = Todo.find({});
+  query.then(function(todos) {
+    console.log(todos)
+    res.json({todos: todos})
+
+  })
+  .catch( function(err) {
+    res.json(500, `ERROR: ${err}`)
+  })
+});
+
+router.post('/', function(req, res){
+  var newToDo;
+
+  Todo.create({
+    description: req.body.description,
+    done: req.body.done || false,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  .then(function(todo){
+    console.log('JUST CREATED TODO OBJECT>>>>', todo)
+    newTodo = todo;
+  })
+  .then(function(){
+    return Todo.find({}).exec();
+  })
+  .then(function(todos) {
+    console.log('ALL TODOS>>>>', todos)
+
+    res.json({todos: todos, todo: newTodo});
+  })
+  .catch(function(err) {
+    res.json(400, err)
+  })
+});
+
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -9,4 +9,39 @@
         .catch((err) => {
           console.log(err);
         });
+
+      // APP STATE
+      // ==============================
+      this.isCreating = false;
+
+      function startCreating() {
+        this.isCreating = true;
+        this.isEditing = false;
+      }
+
+      function reset(todo) {
+        this.isCreating = false;
+        this.isEditing = false;
+      }
+
+      // CRUD LOGIC
+      // ==============================
+      function addTodo(newTodo) {
+
+        $http.post('/todos', newTodo)
+          .then(function(response) {
+            self.todos = response.data.todos;
+
+            newTodo.description = '';
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
+      // PUBLIC METHODS
+      // ==============================
+      this.startCreating = startCreating;
+      this.addTodo = addTodo;
+      this.reset = reset;
 })})();
